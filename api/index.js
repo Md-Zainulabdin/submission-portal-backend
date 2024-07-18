3297790f4211bb43c1b86a5bfe1e43da4a2d86de
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-07-18 16:58:19 +0500

    add user routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import app from './app.js';
-// import connectDB from './db/index.js';
+import connectDB from "./db/index.js";
 
 // Configure dotenv for local development environment variables
 dotenv.config();
@@ -9,7 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 
 // Database connection 
-// connectDB();
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
